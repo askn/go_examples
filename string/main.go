73e7aabf693e07cec1570f98c28edaa17c382faa
aşkın degik <askn@bil.omu.edu.tr>
2013-11-27 02:02:09 +0200

    uniq

--- string/main.go
+++ string/main.go
@@ -12,6 +12,24 @@ func reverse(str string) string {
 	return string(chars)
 }
 
+func uniq(arr []byte) []byte {
+	uniq_arr := []byte{}
+
+	for _, v := range arr {
+		i := 0
+		for _, u := range uniq_arr {
+			if v == u {
+				i++
+			}
+		}
+		if i == 0 {
+			uniq_arr = append(uniq_arr, v)
+		}
+	}
+	return uniq_arr
+}
+
 func main() {
 	fmt.Printf("%s\n", reverse("askin"))
+	fmt.Printf("%s\n", uniq([]byte("asskingedikk")))
 }
