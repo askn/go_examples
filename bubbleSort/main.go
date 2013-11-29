46992f41fed163ee96a7bc539cca5891e273b630
aşkın degik <askn@bil.omu.edu.tr>
2013-11-29 04:24:10 +0200

    bubble sort

---
+++ bubbleSort/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+)
+
+func BubbleSort(list []byte) []byte {
+	for n := len(list); n != 0; {
+		newn := 0
+		for i := 1; i < n; i++ {
+			if list[i-1] > list[i] {
+				list[i-1], list[i] = list[i], list[i-1]
+				newn = i
+			}
+		}
+		n = newn
+		// fmt.Printf("%d\n", n)
+	}
+	return list
+}
+
+func BSort(list []byte) []byte {
+	for i := 1; i < len(list); i++ {
+		for j := len(list) - 1; j >= 1; j-- {
+			if list[j-1] > list[j] {
+				list[j-1], list[j] = list[j], list[j-1]
+			}
+		}
+	}
+	return list
+}
+
+func main() {
+	// 
+	fmt.Printf("%d\n", BSort([]byte("askingedik")))
+	fmt.Printf("%s\n", BSort([]byte("askingedik")))
+	fmt.Printf("%d\n", BubbleSort([]byte{5, 1, 4, 2, 8}))
+	fmt.Printf("%d\n", BSort([]byte{5, 1, 4, 2, 8}))
+}
