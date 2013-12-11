3402ea5957673770e10930253343225637dbae20
aşkın degik <askn@bil.omu.edu.tr>
2013-12-11 02:24:22 +0200

    magic

---
+++ magic/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type Base struct{}
+
+func (Base) Magic() { fmt.Print("base magic ") }
+func (self Base) MoreMagic() {
+	self.Magic()
+	self.Magic()
+}
+
+type Voodoo struct {
+	Base
+}
+
+func (Voodoo) Magic() { fmt.Println("voodoo magic") }
+
+func main() {
+	v := new(Voodoo)
+	v.Magic()
+	v.MoreMagic()
+}
+
+/*
+voodoo magic
+base magic base magic
+*/
