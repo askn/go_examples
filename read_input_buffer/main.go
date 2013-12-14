32f96898238db3112e3848239221eac75dde6e7e
aşkın degik <askn@bil.omu.edu.tr>
2013-12-14 22:19:59 +0200

    read input

---
+++ read_input_buffer/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	inputReader := bufio.NewReader(os.Stdin)
+	fmt.Println("Please enter your name:")
+	input, err := inputReader.ReadString('\n')
+
+	if err != nil {
+		fmt.Println("There were errors reading, exiting program.")
+		return
+	}
+	fmt.Printf("Your name is %s", input)
+}
